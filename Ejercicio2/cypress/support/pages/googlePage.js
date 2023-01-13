e937e07cfcd2ed2525074ce826c4da7d0a00f91f
aferreriblan <119043901+aferreriblan@users.noreply.github.com>
2023-01-13 13:57:08 +0100

    Update googlePage.js

--- Ejercicio2/cypress/support/pages/googlePage.js
+++ Ejercicio2/cypress/support/pages/googlePage.js
@@ -1,39 +1,30 @@
 
 class google{
 
-    
     xpathBotonCookies= "//button/div[contains(text(),'Aceptar todo')]";
     xpathCajaBusqueda= "//input[@name = 'q']";
     xpathResultadoWiki= "//*[contains(text(), 'Wikipedia')]/ancestor::a";
     
     constructor(){
         //this.url = 'variables.baseUrl'
-
     }
     
     navegar = function(url){
         cy.visit(url);
-
     }
     
-
     hacerClick = function(boton){
         cy.xpath(boton).click();
-
     }
     typear = function (busqueda, entrada){
-        
         cy.xpath(busqueda).type(entrada);
         cy.xpath(busqueda).type('{enter}');
-
     }
-
 
     tomarCaptura = function(filename) {
         cy.viewport(1280, 720);
         cy.screenshot(filename);
     }
-
 
 }
 
