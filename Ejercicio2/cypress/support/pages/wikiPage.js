f640cc097c128953f433bf1480ab0041f7692729
aferreriblan <119043901+aferreriblan@users.noreply.github.com>
2023-01-13 13:57:57 +0100

    Update wikiPage.js

--- Ejercicio2/cypress/support/pages/wikiPage.js
+++ Ejercicio2/cypress/support/pages/wikiPage.js
@@ -3,24 +3,17 @@ class wiki{
 
     constructor(){
         //this.url = 'variables.baseUrl'
-
     }
  
-
-
     buscarResultadoWiki = function (palabraClave, filename){
         cy.xpath(palabraClave).scrollIntoView();
         cy.xpath(palabraClave).screenshot(filename);
-        
-
     }
     
     tomarCaptura = function(filename) {
         cy.viewport(1280, 720);
         cy.screenshot(filename);
     }
-
-
 }
 
 export default new wiki();
